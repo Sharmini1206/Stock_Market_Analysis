0cf631319703bdf197f16d5bdb2735cc19e25572
sharmini <sharmini1206@gmail.com>
2023-11-05 22:42:52 +0530

    daily return of the stock on average

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #What was the change in price of the stock overtime?
 
 import matplotlib
+import pdr as pdr
 import pip
 import pandas as pd
 import numpy as np
@@ -101,3 +102,87 @@ axes[1, 1].set_title("AMAZON")
 
 fig.tight_layout()
 plt.show()
+
+# What was the daily return of stock on average
+
+#We'll use pct_change to find the percent change for each day
+for company in company_list:
+    company["Daily Return"] = company["Adj Close"].pct_change()
+
+#Then we'll plot the daily return percentage
+fig, axes = plt.subplots(nrows=2, ncols=2)
+fig.set_figheight(10)
+fig.set_figwidth(15)
+
+AAPL["Daily Return"].plot(ax=axes[0, 0], legend=True,
+                          linestyle="--", marker="o")
+axes[0, 0].set_title("APPLE")
+
+GOOG["Daily Return"].plot(ax=axes[0, 1], legend=True,
+                          linestyle="--", marker="o")
+axes[0, 1].set_title("GOOGLE")
+
+MSFT["Daily Return"].plot(ax=axes[1, 0], legend=True,
+                          linestyle="--", marker="o")
+axes[1, 0].set_title("MICROSOFT")
+
+AMZN["Daily Return"].plot(ax=axes[1, 1], legend=True,
+                          linestyle="--", marker="o")
+axes[1, 1].set_title("AMAZON")
+
+fig.tight_layout()
+plt.show()
+
+#now let's get an overall look
+# at the average daily return using a histogram.
+plt.figure(figsize=(12, 9))
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    company["Daily Return"].hist(bins=50)
+    plt.xlabel("Daily Return")
+    plt.ylabel("Counts")
+    plt.title(f"{column_name[i -1]}")
+
+plt.tight_layout
+plt.show()
+
+#What was the correlation between
+# different stocks closing prices
+
+#Grab all the closing prrices for
+# the techstock list into one dataFrame
+
+closing_df = yf.download(tech_list, start=start,
+                         end=end)["Adj Close"]
+
+#Make a new tech returns DataFrame
+tech_rets = closing_df.pct_change()
+tech_rets.head()
+
+print(tech_rets.head())
+
+#Comparing Google to itself show
+# a perfetly linear relationship
+
+sns.jointplot(x="GOOG", y="GOOG", data=tech_rets,
+              kind="scatter", color="seagreen")
+plt.show()
+
+#We'll use joinplot to compare the daily returns
+# of Google and Microsoft
+
+sns.jointplot(x="GOOG", y="MSFT", data=tech_rets,
+              kind="scatter", color="seagreen")
+plt.show()
+
+#We can simply pairplot on our DataFrame for
+# an automtic visual analysis of all the comparisons
+
+sns.pairplot(tech_rets, kind="reg")
+plt.show()
+
+#set up our figure by naming it returns_fig, call pairplot on the DaataFrame
+returns_fig = sns.PairGrid(tech_rets.dropna())
+plt.show()
+
